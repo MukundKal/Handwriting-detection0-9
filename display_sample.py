c00ecbf0d16913406f4368bc8fde5bc2720d00bf
Mukund Kalra <mukundkalra@gmail.com>
2019-03-26 01:34:23 +0530

    Add files via upload

---
+++ display_sample.py
@@ -0,0 +1,16 @@
+
+import matplotlib.pyplot as plt
+
+
+def display_sample(num):
+    #Print the one-hot array of this sample's label 
+    print(train_labels[num])  
+    #Print the label converted back to a number
+    label = train_labels[num].argmax(axis=0)
+    #Reshape the 768 values to a 28x28 image
+    image = train_images[num].reshape([28,28])
+    plt.title('Sample: %d  Label: %d' % (num, label))
+    plt.imshow(image, cmap=plt.get_cmap('gray_r'))
+    plt.show()
+    
+display_sample(1234)
